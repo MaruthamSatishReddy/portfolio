77150ec16d25659b0c5d1cc26f1981bf7d403714
MaruthamSatishReddy <109795642+MaruthamSatishReddy@users.noreply.github.com>
2022-11-03 23:29:40 +0530

    sanity

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -3,7 +3,7 @@ import { groq } from 'next-sanity';
 import { sanityClient } from '../../sanity';
 import { PageInfo } from '../../typings';
 const query = groq`
-*[_type=="pageInfo"][0]`;
+*[_type == "pageInfo"][0]`;
 
 type Data = { pageInfo: PageInfo };
 export default async function handler(
